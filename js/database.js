52504c56e9ba7b788d97d292744a65e4a09c3851
JoshuaShin <joshua_shin@hotmail.com>
2019-03-14 18:52:43 -0700

    Create create_recipe page
    Create firebase database

---
+++ js/database.js
@@ -0,0 +1,12 @@
+src="https://www.gstatic.com/firebasejs/5.9.0/firebase.js"
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBrGlVFqo2ixWMzeeq6ppt4jaMErC8H5LY",
+    authDomain: "foodle-d4b88.firebaseapp.com",
+    databaseURL: "https://foodle-d4b88.firebaseio.com",
+    projectId: "foodle-d4b88",
+    storageBucket: "foodle-d4b88.appspot.com",
+    messagingSenderId: "506441498159"
+};
+firebase.initializeApp(config);
