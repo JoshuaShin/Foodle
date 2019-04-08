22f78593a4657b07bc87fca62b01e0b8a07691eb
JoshuaShin <joshua_shin@hotmail.com>
2019-04-07 20:22:50 -0700

    Update profile and background image

---
+++ js/ingredient_type_database.js
@@ -0,0 +1,2 @@
+let meatandfish = ["chicken", "pork", "beef", "salmon", "tuna", "turkey", "chicken breast", "butter", "cheese", "lamb", "fish"];
+let liquid = ["galic sauce","olive oil", "oliveoil", "oil", "coconut milk", "coconutmilk", "milk", "cream", "tomato sauce","tomatosauce", "teriyaki sauce", "teriyakisauce", "teriyaki"];
